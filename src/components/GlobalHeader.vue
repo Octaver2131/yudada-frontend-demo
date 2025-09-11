45c5d579a767cf9d14366809601fe762d14fc539
Octaver <Octaver2131@outlook.com>
2025-09-11 10:44:47 +0800

    全局 Header 组件 v1

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,71 @@
+<template>
+  <a-row id="globalHeader" align="center" :wrap="false">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="titleBar">
+            <img class="logo" src="../assets/logo.png" />
+            <div class="title">1111</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in routes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>
+        <a-button type="primary" href="/user/login">Login</a-button>
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+
+// 当前选中的菜单项
+const selectedKeys = ref(["/"]);
+// 路由跳转时，自动更新选中的菜单项
+router.afterEach((to, from, next) => {
+  selectedKeys.value = [to.path];
+})
+
+// 点击菜单跳转到对应页面
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+
+<style scoped>
+#globalHeader {
+}
+.titleBar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  margin-left: 16px;
+}
+
+.logo {
+  width: 48px;
+  height: 48px;
+}
+</style>
