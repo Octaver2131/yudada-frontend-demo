430e3f73b5df00b1177e83c48da73cdb953c2fb9
Octaver <Octaver2131@outlook.com>
2025-09-12 15:46:35 +0800

    实现 全局入口函数

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <template v-if="route.path.startsWith('/user')">
+      <router-view />
+    </template>
+    <template v-else>
+      <BasicLayout />
+    </template>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout.vue";
+import { useRoute } from "vue-router";
+import { onMounted } from "vue";
 
-nav {
-  padding: 30px;
-}
+const route = useRoute();
+/**
+ * 全局初始化函数，有全局单次调用的代码，都可以写到这里
+ */
+const doInit = () => {
+  console.log("hello 欢迎来到我的项目");
+};
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+onMounted(() => {
+  doInit();
+});
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+// getLoginUserUsingGet().then((res) => {
+//   console.log(res);
+// });
+</script>
+
+<style scoped></style>
