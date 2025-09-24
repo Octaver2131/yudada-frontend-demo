56c53fde1246365afc833dfdc8585bd7384563d1
Octaver <Octaver2131@outlook.com>
2025-09-24 17:44:33 +0800

    feat: 实现 AI 生成题目抽屉

--- src/views/add/AddQuestionPage.vue
+++ src/views/add/AddQuestionPage.vue
@@ -8,11 +8,18 @@
       auto-label-width
       @submit="handleSubmit"
     >
-      <a-form-item label="应用 ID"> {{ appId }}} </a-form-item>
+      <a-form-item label="应用 ID"> {{ appId }} </a-form-item>
       <a-form-item label="题目列表" :content-flex="false" :merge-props="false">
-        <a-button @click="addQuestion(questionContent.length)">
-          底部添加题目
-        </a-button>
+        <a-space size="medium">
+          <a-button @click="addQuestion(questionContent.length)">
+            底部添加题目
+          </a-button>
+          <!-- AI 生成抽屉 -->
+          <AiGenerateQuestionDrawer
+            :appId="appId"
+            :onSuccess="onAiGenerateSuccess"
+          />
+        </a-space>
         <!--        遍历每道题目-->
         <div v-for="(question, index) in questionContent" :key="index">
           <a-space size="large">
@@ -101,6 +108,7 @@ import {
   editQuestionUsingPost,
   listQuestionVoByPageUsingPost,
 } from "@/api/questionController";
+import AiGenerateQuestionDrawer from "@/views/add/components/AiGenerateQuestionDrawer.vue";
 
 interface Props {
   appId: string;
@@ -224,5 +232,14 @@ const handleSubmit = async () => {
   } else {
     message.error("操作失败，" + res.data.message);
   }
+};
+
+/**
+ * AI 生成题目成功后执行
+ * @param question
+ */
+const onAiGenerateSuccess = (result: API.QuestionContentDTO[]) => {
+  message.success(`AI 生成题目成功，生成 ${result.length} 道题目`);
+  questionContent.value = [...questionContent.value, ...result];
 };
 </script>
