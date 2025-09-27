fe6230f58dbac3ea9bc0a3296e5208e834a8eb6e
Octaver <Octaver2131@outlook.com>
2025-09-27 16:50:41 +0800

    feat: 进入页面生成唯一 id

--- src/views/answer/DoAnswerPage.vue
+++ src/views/answer/DoAnswerPage.vue
@@ -62,7 +62,10 @@ import {
   listQuestionVoByPageUsingPost,
 } from "@/api/questionController";
 import { getAppVoByIdUsingGet } from "@/api/appController";
-import { addUserAnswerUsingPost } from "@/api/userAnswerController";
+import {
+  addUserAnswerUsingPost,
+  generateUserAnswerIdUsingGet,
+} from "@/api/userAnswerController";
 
 interface Props {
   appId: string;
@@ -106,6 +109,23 @@ const answerList = reactive<string[]>([]);
 // 是否正在提交中
 const submitting = ref(false);
 
+// 唯一 id
+const id = ref<number>();
+
+// 生成唯一 id
+const generateId = async () => {
+  const res = await generateUserAnswerIdUsingGet();
+  if (res.data.code === 0) {
+    id.value = res.data.data as any;
+  } else {
+    message.error("获取唯一 ID 失败，" + res.data.message);
+  }
+};
+
+// 进入页面时，生成唯一 id
+watchEffect(() => {
+  generateId();
+});
 /**
  * 加载数据
  */
@@ -168,6 +188,7 @@ const doSubmit = async () => {
   const res = await addUserAnswerUsingPost({
     appId: props.appId as any,
     choices: answerList,
+    id: id.value as any,
   });
   if (res.data.code === 0 && res.data.data) {
     router.push(`/answer/result/${res.data.data}`);
